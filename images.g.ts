0d872a1758b589c28160dd52262cb98729a608b6
OmarIdrisT <146827333+OmarIdrisT@users.noreply.github.com>
2024-12-01 22:27:45 +0100

    Menú e intro añadidos

--- images.g.ts
+++ images.g.ts
@@ -1,4 +1,4 @@
-// Auto-generated code. Do not edit.
+// Código generado automáticamente. No editar.
 namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
@@ -487,4 +487,4 @@ e e 2 2 2 2 2 2 2 2 2 e e . . .
     })
 
 }
-// Auto-generated code. Do not edit.
+// Código generado automáticamente. No editar.
