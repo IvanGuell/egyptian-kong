83a1ccdd1fb155839ba33503bd45dd2b0d048c0f
IvanGuell <146827049+IvanGuell@users.noreply.github.com>
2024-12-02 15:15:07 +0100

    ee

--- main.ts
+++ main.ts
@@ -19,39 +19,40 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         }
     }
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.princess, function (sprite, otherSprite) {
-    effects.confetti.startScreenEffect()
-    game.gameOver(true)
-})
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    rario.setImage(assets.image`RarioL`)
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`stair1`, function (sprite, location) {
-    if (controller.up.isPressed()) {
-        rario.setVelocity(0, -50)
-    }
-})
-scene.onOverlapTile(SpriteKind.Projectile, sprites.dungeon.stairSouth, function (sprite, location) {
-    sprite.setVelocity(-50, 0)
-    sprite.ay = 500
-})
-scene.onOverlapTile(SpriteKind.barril, sprites.dungeon.stairSouth, function (sprite, location) {
-    sprite.setVelocity(-50, 0)
-    sprite.ay = 500
-})
-scene.onOverlapTile(SpriteKind.barril, assets.tile`stair2`, function (sprite, location) {
-    sprite.setVelocity(50, 0)
-    sprite.ay = 500
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    rario.setImage(assets.image`Rario`)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite7, otherSprite3) {
     sprites.destroy(rario)
     effects.blizzard.startScreenEffect()
     game.gameOver(false)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`stair2`, function (sprite, location) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.princess, function (sprite2, otherSprite2) {
+    effects.confetti.startScreenEffect()
+    game.gameOver(true)
+})
+scene.onOverlapTile(SpriteKind.barril, sprites.dungeon.stairSouth, function (sprite5, location3) {
+    sprite5.setVelocity(-50, 0)
+    sprite5.ay = 500
+})
+scene.onOverlapTile(SpriteKind.barril, assets.tile`stair2`, function (sprite6, location4) {
+    sprite6.setVelocity(50, 0)
+    sprite6.ay = 500
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.barril, function (sprite9, otherSprite4) {
+    sprites.destroy(rario)
+    effects.blizzard.startScreenEffect()
+    game.gameOver(false)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    rario.setImage(assets.image`RarioL`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`stair2`, function (sprite8, location5) {
+    if (controller.up.isPressed()) {
+        rario.setVelocity(0, -50)
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    rario.setImage(assets.image`Rario`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`stair1`, function (sprite3, location) {
     if (controller.up.isPressed()) {
         rario.setVelocity(0, -50)
     }
@@ -59,17 +60,16 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`stair2`, function (sprite, lo
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     rario.setImage(assets.image`Rario`)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.barril, function (sprite, otherSprite) {
-    sprites.destroy(rario)
-    effects.blizzard.startScreenEffect()
-    game.gameOver(false)
+scene.onOverlapTile(SpriteKind.Projectile, sprites.dungeon.stairSouth, function (sprite4, location2) {
+    sprite4.setVelocity(-50, 0)
+    sprite4.ay = 500
 })
-scene.onOverlapTile(SpriteKind.Projectile, assets.tile`stair2`, function (sprite, location) {
-    sprite.setVelocity(50, 0)
-    sprite.ay = 500
+scene.onOverlapTile(SpriteKind.Projectile, assets.tile`stair2`, function (sprite10, location6) {
+    sprite10.setVelocity(50, 0)
+    sprite10.ay = 500
 })
-let barrilP: Sprite = null
 let barrilR: Sprite = null
+let barrilP: Sprite = null
 let rario: Sprite = null
 rario = sprites.create(assets.image`Rario`, SpriteKind.Player)
 rario.setPosition(6, 615)
@@ -89,18 +89,18 @@ game.showLongText("Este juego intenta recrear la experiencia exacta de ese clasi
 forever(function () {
     for (let index = 0; index < 999999; index++) {
         pause(3000)
-        barrilR = sprites.create(assets.image`barril1`, SpriteKind.barril)
-        tiles.placeOnRandomTile(barrilR, sprites.dungeon.chestClosed)
-        barrilR.setVelocity(-50, 0)
-        barrilR.ay = 500
+        barrilP = sprites.create(assets.image`barrilP`, SpriteKind.Projectile)
+        tiles.placeOnRandomTile(barrilP, sprites.dungeon.chestOpen)
+        barrilP.setVelocity(50, 0)
+        barrilP.ay = 500
     }
 })
 forever(function () {
     for (let index = 0; index < 999999; index++) {
         pause(3000)
-        barrilP = sprites.create(assets.image`barrilP`, SpriteKind.Projectile)
-        tiles.placeOnRandomTile(barrilP, sprites.dungeon.chestOpen)
-        barrilP.setVelocity(50, 0)
-        barrilP.ay = 500
+        barrilR = sprites.create(assets.image`barril1`, SpriteKind.barril)
+        tiles.placeOnRandomTile(barrilR, sprites.dungeon.chestClosed)
+        barrilR.setVelocity(-50, 0)
+        barrilR.ay = 500
     }
 })
